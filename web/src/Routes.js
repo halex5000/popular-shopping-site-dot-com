2026dbb220425b7a7396c50a8f44342613a03c5a
Alex Hardman <alexhardman@launchdarkly.com>
2022-06-22 12:36:13 -0400

    adding dbauth with login/signup/etc

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 
 import HomeLayout from 'src/layouts/HomeLayout'
 import ProductsLayout from 'src/layouts/ProductsLayout'
@@ -15,15 +15,21 @@ import ProductsLayout from 'src/layouts/ProductsLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={HomeLayout}>
         <Route path="/" page={HomePage} name="home" />
       </Set>
-      <Set wrap={ProductsLayout}>
-        <Route path="/products/new" page={ProductNewProductPage} name="newProduct" />
-        <Route path="/products/{id}/edit" page={ProductEditProductPage} name="editProduct" />
-        <Route path="/products/{id}" page={ProductProductPage} name="product" />
-        <Route path="/products" page={ProductProductsPage} name="products" />
-      </Set>
+      <Private unauthenticated="home">
+        <Set wrap={ProductsLayout}>
+          <Route path="/products/new" page={ProductNewProductPage} name="newProduct" />
+          <Route path="/products/{id}/edit" page={ProductEditProductPage} name="editProduct" />
+          <Route path="/products/{id}" page={ProductProductPage} name="product" />
+          <Route path="/products" page={ProductProductsPage} name="products" />
+        </Set>
+      </Private>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/layouts/HomeLayout/HomeLayout.js
+++ web/src/layouts/HomeLayout/HomeLayout.js
@@ -1,11 +1,28 @@
 import { Container } from '@mui/material'
 
+import { useAuth } from '@redwoodjs/auth'
+import { routes, Link } from '@redwoodjs/router'
+
 const HomeLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
   return (
     <>
       <Container>
         <header>
-          <h1 className="header">Popular Shopping Site Dot Com</h1>
+          <div className="flex-between">
+            <h1 className="header">Popular Shopping Site Dot Com</h1>
+            {isAuthenticated ? (
+              <div>
+                <span>Logged in as {currentUser.email}</span>{' '}
+                <button type="button" onClick={logOut}>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <Link to={routes.login()}>Login</Link>
+            )}
+          </div>
           <nav>
             <ul>
               <li>{/* <p>something</p> */}</li>
