52de7facc10baa35fb8c841b1c566031a3a83af5
Alex Hardman <alexhardman@launchdarkly.com>
2022-06-22 11:54:00 -0400

    migrating to postgres

--- api/db/migrations/20220621194454_create_product/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
